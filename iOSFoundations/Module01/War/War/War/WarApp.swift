76dce8ee81af7dfd0a13a26c9630c91d86a28652
Bern N <bernnystrom@gmail.com>
2023-10-05 22:58:42 +0800

    🎉 (new): Initial commit
    First git commit of the War app project with its image assets.

---
+++ iOSFoundations/Module01/War/War/War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Bern N on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
