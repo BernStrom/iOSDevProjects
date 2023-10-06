b8dba7ba4df390755fda1374be423fa10d6bcd76
Bern N <bernnystrom@gmail.com>
2023-10-07 01:04:32 +0800

    🏗️ (new): Create new file to model menu items data
    A new file "MenuItem" was created to set the data model of the menu item details.

---
+++ iOSFoundations/Project02/Menu/Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Bern N on 10/7/23.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
