a4788334908e146c7eaf6fb513d4ff7d581e5100
Bern N <bernnystrom@gmail.com>
2023-10-07 00:48:07 +0800

    🎉 (new): Initial commit
    First git commit of the Menu app project, along with its image assets.

---
+++ iOSFoundations/Project02/Menu/Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Bern N on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
---
+++ iOSFoundations/Project02/Menu/Menu/MenuView.swift
@@ -0,0 +1,24 @@
+//
+//  MenuView.swift
+//  Menu
+//
+//  Created by Bern N on 10/7/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MenuView()
+}
