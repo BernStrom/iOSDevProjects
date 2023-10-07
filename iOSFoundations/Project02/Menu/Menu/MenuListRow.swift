59f89440e33c233f3cf002c453011c40e168e541
Bern N <bernnystrom@gmail.com>
2023-10-07 20:27:08 +0800

    ♻️ (refactor): Extract menu items row into its own custom view
    Moved the row of menu list items into a separate custom view file named "MenuListRow".

---
+++ iOSFoundations/Project02/Menu/Menu/MenuListRow.swift
@@ -0,0 +1,38 @@
+//
+//  MenuListRow.swift
+//  Menu
+//
+//  Created by Bern N on 10/7/23.
+//
+
+import SwiftUI
+
+struct MenuListRow: View {
+    var item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 50)
+                .cornerRadius(10)
+            
+            Text(item.name)
+                .bold()
+            
+            Spacer()
+            
+            Text("$\(item.price)")
+        }
+        .listRowSeparator(.hidden)
+        .listRowBackground(
+            Color(.brown)
+                .opacity(0.1)
+        )
+    }
+}
+
+#Preview {
+    MenuListRow(item: MenuItem(name: "Test Item", price: "2.99", imageName: "tako-sushi"))
+}
