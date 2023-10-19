a1ab9600da00b4aa58fbf439700a42d1e13b6a98
Bern N <bernnystrom@gmail.com>
2023-10-19 20:46:06 +0800

    🎉 (new): Initial commit

---
+++ iOSNetworking/Project01/Citysights/Citysights/CitysightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitysightsApp.swift
+//  Citysights
+//
+//  Created by Bern N on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CitysightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSNetworking/Project01/Citysights/Citysights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Citysights
+//
+//  Created by Bern N on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
