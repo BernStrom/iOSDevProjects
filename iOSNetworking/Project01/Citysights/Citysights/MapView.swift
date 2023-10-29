1713cfda01ecbfe6a3aa88c1e3a5a4a46b435682
Bern N <bernnystrom@gmail.com>
2023-10-29 20:30:23 +0800

    ✨ (feat): Add map markers to represent businesses in MapView
    Selection of the businesses are detected and set accordingly to update the business property of the business model.

--- iOSNetworking/Project01/Citysights/Citysights/MapView.swift
+++ iOSNetworking/Project01/Citysights/Citysights/MapView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @Environment(BusinessModel.self) private var model
+    @State private var selectedBusinessId: String?
+    
     var body: some View {
-        Map()
+        Map(selection: $selectedBusinessId) {
+            ForEach(model.businesses) { business in
+                Marker(business.name ?? "Restaurant", coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0, longitude: business.coordinates?.longitude ?? 0))
+                    .tag(business.id ?? "None")
+            }
+        }
+        .onChange(of: selectedBusinessId) { oldValue, newValue in
+            let business = model.businesses.first { business in
+                business.id == selectedBusinessId
+            }
+            
+            if let business = business {
+                model.selectedBusiness = business
+            }
+        }
     }
 }
 
 #Preview {
     MapView()
+        .environment(BusinessModel())
 }
