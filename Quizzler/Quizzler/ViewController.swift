fa88b1e0e09bbcd4e2dbf8983247a136ffd1f4a6
Bern N <bernnystrom@gmail.com>
2023-11-20 01:47:50 +0800

    💄 (style): Set table cells and labels to display actual question and answers
    Question & answers are set to reload and display the next question & answer, after the user has answered the current one. A prompt/notification will be implemented next to notify the user whether they've got the answers correct or wrong.

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
     var model = QuizModel()
     var questions = [Question]()
     var currentQuestionIndex = 0
+    var numCorrect = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +28,25 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
         model.getQuestions()
     }
     
+    func displayQuestion() {
+        guard questions.count > 0 && currentQuestionIndex < questions.count else {
+            return
+        }
+        
+        // Display the question text
+        questionLabel.text = questions[currentQuestionIndex].question
+        
+        // Reload the answers table
+        tableView.reloadData()
+    }
+    
     // MARK: - QuizProtocol Methods
     
     func questionsRetrieved(_ questions: [Question]) {
         self.questions = questions
         
-        tableView.reloadData()
+        // Display the first question
+        displayQuestion()
     }
     
     // MARK: - UITableView Delegate Methods
@@ -57,10 +71,32 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITab
         let label = cell.viewWithTag(1) as? UILabel
         
         if label != nil {
-            // TODO: Set the answer text for the label
+            let question = questions[currentQuestionIndex]
+            
+            if question.answers != nil && indexPath.row < question.answers!.count {
+                // Set the answer text for the label
+                label!.text = question.answers![indexPath.row]
+            }
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let question = questions[currentQuestionIndex]
+        
+        // Check if user has tapped on the right answer
+        if question.correctAnswerIndex! == indexPath.row {
+            print("User got the correct answer.")
+        } else {
+            print("User got the wrong answer.")
+        }
+        
+        // Increment the currentQuestionIndex
+        currentQuestionIndex += 1
+        
+        // Display the next question
+        displayQuestion()
     }
 
 }
