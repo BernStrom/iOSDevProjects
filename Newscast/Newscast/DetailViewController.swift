ef2dfd1dd17cc386550e4753e7640bb3e0cd8110
Bern N <bernnystrom@gmail.com>
2023-11-22 00:31:27 +0800

    🚧 (style): Add view controllers connection setup to UI
    Added the tableView and webView IBOutlet connections and protocol stubs for the UITableView to the view controllers.

---
+++ Newscast/Newscast/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Newscast
+//
+//  Created by Bern N on 11/21/23.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Newscast/Newscast/ViewController.swift
+++ Newscast/Newscast/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var model = ArticleModel()
     var articles = [Article]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         model.delegate = self
         model.getArticles()
@@ -21,12 +26,38 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - UITableView Protocol Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath)
+        
+        // TODO: Customize table cell
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+}
+
 extension ViewController: ArticleModelProtocol {
     
-    // MARK: - Article Model Protocol Methods
+    // MARK: - ArticleModel Protocol Methods
     
     func articlesRetrieved(_ articles: [Article]) {
         self.articles = articles
+        
+        // Refresh the tableView to populate it with the news articles data
+        tableView.reloadData()
     }
     
 }
